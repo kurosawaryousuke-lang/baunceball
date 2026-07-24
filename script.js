572b81c68019a07f76c83eac090766d00dc92e3c
kurosawaryousuke-lang <kurosawa.ryousuke@tsuchiura1-h.ibk.ed.jp>
2026-07-24 21:47:41 +0900

    n

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const stage = {
 
     stars: [
         { x: 200, y: 100, got: false },
-        { x: 700, y: 500, got: false }
+        { x: 700, y: 500, got: false },
     ]
     spikes: [
     { x: 450, y: 250, r: 15 },
